caaf4f09afd629c94cc6efc055396346eb6770e3
Ludovic Claude <ludovic.claude54@gmail.com>
2018-04-25 15:35:10 +0200

    Use JAVA_OPTIONS to include memory settings in Docker

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export LIBPROCESS_PORT="${PORT1}"
-exec java $JVM_OPTS $JAVA_OPTS -jar /chronos/chronos.jar $@ --http_port $PORT0
+exec java $JVM_OPTS ${JAVA_OPTIONS} -jar /chronos/chronos.jar $@ --http_port $PORT0
